483d63ebe400ed9926300b44f58433a5bfe35f5b
digital <digital@iMac24-5.local>
2022-11-30 15:44:19 +0100

    init

---
+++ Observable/ObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableApp.swift
+//  Observable
+//
+//  Created by digital on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
